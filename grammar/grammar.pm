cd0766bf9b3949ead75b94ed8789f769c0a1556d
David Oswald <davido@cpan.org>
2012-03-07 11:16:45 -0800

    Version 0.38_002: Bugfixes introduced in refactoring.

--- grammar/grammar.pm
+++ grammar/grammar.pm
@@ -8,7 +8,7 @@ use vars qw($TYPEMAP_KIND $class_part $class_decl $star);
 # Dev versions will have a _0xx suffix.
 # We eval the $VERSION to accommodate dev version numbering as described in
 # perldoc perlmodstyle
-our $VERSION = '0.38_001';
+our $VERSION = '0.38_002';
 $VERSION = eval $VERSION;  ## no critic (eval)
 
 # Parse::RecDescent 1.90 and later have an incompatible change
