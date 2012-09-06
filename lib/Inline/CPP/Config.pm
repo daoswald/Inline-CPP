38a7ad2aaf753a4ffdb74f3772461ab8a5455c34
David Oswald <daoswald@gmail.com>
2012-09-05 19:20:04 -0700

    Prepare CPAN release v0.41

--- lib/Inline/CPP/Config.pm
+++ lib/Inline/CPP/Config.pm
@@ -5,8 +5,8 @@ package Inline::CPP::Config;
 use strict;
 use warnings;
 
-our $VERSION = '0.40_001';
-$VERSION = eval $VERSION; ## no critic (eval)
+our $VERSION = '0.41';
+#$VERSION = eval $VERSION; ## no critic (eval)
 
 
 # DO NOT MANUALLY ALTER THE FOLLOWING TWO LINES: Makefile.PL locates them by
